2f79c8ab67d2eaa71fc12315066096713faa8c89
glen <gulyashshsh@gmail.com>
2023-07-01 11:59:44 +0600

    added users/me/ url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,7 +6,8 @@ from rest_framework_simplejwt.views import (
 )
 
 from apps.users.views import (
-    UserMeAPIView,
+    UserView,
+    UserMeView,
     UserListView,
     PositionView,
     RegistrationView,
@@ -18,7 +19,8 @@ router.register("positions", PositionView)
 
 urlpatterns = [
     path("users/", UserListView.as_view()),
-    path("users/me/", UserMeAPIView.as_view()),
+    path("users/<int:pk>", UserView.as_view()),
+    path("users/me/", UserMeView.as_view()),
     path("auth/register/", RegistrationView.as_view()),
     path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
